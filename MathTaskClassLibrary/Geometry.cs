612f8c2adec581f25b7ff4a17a57dd2febd031e5
PIBO0121012 Донгак Аркадий Хеймер-оолович <PIBO0121012@sibupk.su>
2022-12-30 01:41:20 +0700

    https://learn.microsoft.com/ru-ru/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2019
    https://metanit.com/sharp/aspnet5/22.1.php
    
    https://vc.ru/dev/137335-unit-testy-na-c
    
    https://habr.com/ru/post/98062/
    
    https://www.cyberforum.ru/csharp-beginners/thread1057776.html
    
    https://www.cyberforum.ru/python-beginners/thread1848587.html
    
    основные источники вдохновления решения поставленных задач тут Судя по работоспособности программы и зеленым галочкам тестов все работает

--- MathTaskClassLibrary/Geometry.cs
+++ MathTaskClassLibrary/Geometry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace MathTaskClassLibrary
 {
@@ -8,6 +9,103 @@ namespace MathTaskClassLibrary
         {
             if (a < 0 || b < 0) throw new System.ArgumentException();
             return a * b;
+        }
+    }
+    public class Simvol
+    {
+        public string Write(int n)
+        {
+            if (n < 1 || n > 26)
+            {
+                throw new ArgumentException();
+            }
+            string str = "";
+            for (char c = 'A'; c < 'A' + n; c++)
+            {
+                str += c;
+            }
+            return str;
+        }
+    }
+    public class Quad
+    {
+        private double a, b, c;
+        public Quad(double a, double b, double c)
+        {
+            if (a == 0) throw new ArgumentException();
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+        public double[] Solution()
+        {
+            double D = b * b - (4 * a * c);
+            if (D > 0)
+            {
+                double x1 = (-b + Math.Sqrt(D)) / 2;
+                double x2 = (-b - Math.Sqrt(D)) / 2;
+                Math.Round(x1, 2);
+                Math.Round(-x2, 2);
+                return new double[] { x1, x2 };
+            }
+            else if (D == 0)
+            {
+                double x = (-b - Math.Sqrt(D)) / 2;
+                Math.Round(x, 2);
+                return new double[] { x };
+            }
+            else
+            {
+                return new double[0];
+            }
+        }
+    }
+    public class Visokos
+    {
+        int year;
+        public Visokos(int year)
+        {
+            if (year < 1)
+            {
+                throw new ArgumentException();
+            }
+            this.year = year;
+        }
+        public int NumDays(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if ((year % 100 == 0) && (year % 400 != 0))
+                    return 365;
+                else
+                    return 366;
+            }
+            else
+                return 365;
+        }
+    }
+    public class Regular
+    {
+        Regex regex = new Regex(@"^[\w\-]+@((\w+)|(\w[\w-]+))\.\w+$");
+        public bool Email(string line)
+        {
+            return regex.IsMatch(line);
+        }
+    }
+    public class Number
+    {
+        public int Sum(string str)
+        {
+            int sum = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] < '0' || str[i] > '9')
+                {
+                    throw new ArgumentException();
+                }
+                sum += str[i] - '0';
+            }
+            return sum;
         }
     }
 }
--- MathTaskClassLibraryTests/UnitTest1.cs
+++ MathTaskClassLibraryTests/UnitTest1.cs
@@ -49,4 +49,203 @@ namespace MathTaskClassLibraryTests
             Assert.ThrowsException<ArgumentException>(()=>g.CalculateArea(a, b),"не обработанные отрицательные стороны");
         }
     }
+
+    [TestClass]
+    public class Simvol
+    {
+        [TestMethod]
+        public void SymvolTest1()
+        {
+            int n = 5;
+            string expected = "ABCDE";
+            MathTaskClassLibrary.Simvol str = new MathTaskClassLibrary.Simvol();
+            string actual = str.Write(n);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SymvolTest2()
+        {
+            bool cathed = false;
+            try
+            {
+                int n = 27;
+                MathTaskClassLibrary.Simvol str = new MathTaskClassLibrary.Simvol();
+                string actual = str.Write(n);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+
+            Assert.IsTrue(cathed, "АргументЭксэпшн");
+        }
+        [TestMethod]
+        public void SymvolTest3()
+        {
+            bool cathed = false;
+            try
+            {
+                int n = 0;
+                MathTaskClassLibrary.Simvol str = new MathTaskClassLibrary.Simvol();
+                string actual = str.Write(n);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+
+            Assert.IsTrue(cathed, "АргументЭксэпшн");
+        }
+    }
+
+    [TestClass]
+    public class Quad
+    {
+        [TestMethod]
+        public void QuadTest1()
+        {
+            double a = 1, b = 1, c = 1;
+            var eq = new MathTaskClassLibrary.Quad(a, b, c);
+            double[] actual = eq.Solution();
+            double[] expected = new double[0];
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void QuadTest2()
+        {
+            //1 тест ?
+            bool cathed = false;
+            try
+            {
+                double a = 0, b = 1, c = 1;
+                var eq = new MathTaskClassLibrary.Quad(a, b, c);
+                double[] actual = eq.Solution();
+                double[] expected = { };
+            }
+            catch
+            {
+                cathed = true;
+            }
+            Assert.IsTrue(cathed);
+        }
+        [TestMethod]
+        public void QuadTest3()
+        {
+            double a = 1, b = -6, c = 9;
+            var eq = new MathTaskClassLibrary.Quad(a, b, c);
+            double[] actual = eq.Solution();
+            double[] expected = { 3 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void QuadTest4()
+        {
+            double a = 1, b = 4, c = 3;
+            var eq = new MathTaskClassLibrary.Quad(a, b, c);
+            double[] actual = eq.Solution();
+            double[] expected = { -1, -3 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+    [TestClass]
+    public class VisicosTest
+    {
+        [TestMethod]
+        public void Year1()
+        {
+            int year = 300;
+            Visokos v = new Visokos(year);
+            int actual = v.NumDays(year);
+            int expected = 365;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Year2()
+        {
+            int year = 1200;
+            Visokos v = new Visokos(year);
+            int actual = v.NumDays(year);
+            int expected = 366;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Year3()
+        {
+            bool cathed = false;
+            try
+            {
+                int year = 0;
+                Visokos v = new Visokos(year);
+                int actual = v.NumDays(year);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+            Assert.IsTrue(cathed);
+        }
+    }
+    [TestClass]
+    public class RegularTest
+    {
+        Regular r = new Regular();
+        [TestMethod]
+        public void IsEmailTest1()
+        {
+            string line = "test@test.test";
+            Assert.IsTrue(r.Email(line));
+        }
+        [TestMethod]
+        public void IsEmailTest2()
+        {
+            string line = "test@testtest";
+            Assert.IsFalse(r.Email(line));
+        }
+    }
+    [TestClass]
+    public class NumberTest
+    {
+        string str;
+        Number a = new Number();
+        [TestMethod]
+        public void SumTest1()
+        {
+            str = "123";
+            int actual = a.Sum(str);
+            int expected = 6;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void SumTest2()
+        {
+            bool cathed = false;
+            try
+            {
+                str = "afs";
+                int actual = a.Sum(str);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+
+            Assert.IsTrue(cathed);
+        }
+        [TestMethod]
+        public void SumTest3()
+        {
+            bool cathed = false;
+            try
+            {
+                str = "-1";
+                int actual = a.Sum(str);
+            }
+            catch (ArgumentException)
+            {
+                cathed = true;
+            }
+
+            Assert.IsTrue(cathed);
+        }
+    }
 }
